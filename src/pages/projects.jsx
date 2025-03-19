9300f096038b4ec055fef8a165bc65875cad0302
abbosweb1 <mirabbosabdusattorov37@gmail.com>
2025-03-19 21:18:14 +0500

    created services

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,30 @@
-
+import img from "../assets/Group 63.png"
+import "./projects.css"
 
 function Projects(){
     return(
         <>
-        <h1>projects</h1>
+        <div className="projectWrapper">
+            <div className="project">
+                <div className="projectLeft">
+                    <img src={img} alt="" />
+                </div>
+                <div className="projectRight">
+                    <p>Projects</p>
+                    <h1>Interioris The Will
+                    of An Epoch Mextreo</h1>
+                    <p>There are many variations of passages of Lorem Ipsum available,
+but the majority have suffered alteration in some form injected
+humour, or randomised words which don't look even slightly
+believable.If you are going to use a passage of Lorem Ipsum,
+sure there isn't anything embarrassing hidden the middleof text.
+All the Lorem Ipsum generators on the Internettend to repeat
+predefined chunks as necessary,making this the first true
+generator on the Internet.</p>
+                    <button>Learn More</button>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,8 +1,21 @@
-
+import "./services.css"
+import Img from "../assets/Group 78.png"
 function Services(){
     return(
         <>
-        <h1>Services</h1>
+        <div className="servicesWrapper">
+            <div className="services">
+                <div className="servicesLeft">
+                    <h1>20 <br /> <span>Years Of Successful
+Working
+The Market</span></h1>
+                    <button>Read More</button>
+                </div>
+                <div className="servicesRight">
+                    <img src={Img} alt="" />
+                </div>
+            </div>
+        </div>
         </>
     )
 }
